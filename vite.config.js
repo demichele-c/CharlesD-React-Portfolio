62d1b693bbc029b612febef12cef09dc542b564a
demichele-c <demichele.charles@yahoo.com>
2024-10-07 13:58:25 -0400

    updated file structure

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist',  // Ensure the build output directory is 'dist'
+  },
+  server: {
+    open: true,  // Automatically open the app in the browser on start
+  },
+  base: '/', // Set the base URL
 })
